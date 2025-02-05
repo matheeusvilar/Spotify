52f4540542ba94b20a09e3f4bc5092f332a353e2
matheeusvilar <morizono597@gmail.com>
2025-02-05 13:35:27 -0300

    Adicionado o JavaScript e integração de API

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 const searchInput = document.getElementById('search-input');
-aacon
+const resultsArtists = Document.getElementById('result-artist');
+const resultPLaylist = document.getElementById ('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result) )
+}
+
+function displayResults(result) {
+    resultPLaylist.classList.add('hidden');
+    const artistsName = document.getElementById('artists-name')
+    const artistsImage = document.getElementById('artists-img')
+
+    result.array.forEach(element => {
+        artistsName.innerText = element.name;
+        artistsImage.src = element.urlImg;
+    });
+
+    resultsArtists.classList.remove('hidden');
+}
+
+document.addEventListener ('input', function(){
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === ''){
+        resultPLaylist.classList.add('hidden');
+        resultsArtists;classList.remove('hidden');
+        return;
+    }
+
+    requestApi(searchTerm);
+})
